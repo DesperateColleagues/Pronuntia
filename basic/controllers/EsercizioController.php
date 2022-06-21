5bc674ff5fa08a620c455a7116842f2ee00b1dde
Michelangelo De Pascale <69792649+MichelangeloDePascale02@users.noreply.github.com>
2022-06-21 20:20:31 +0200

    Added a first version of the creation of the exercises

--- basic/controllers/EsercizioController.php
+++ basic/controllers/EsercizioController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\EsercizioModel;
 use app\models\ImmagineEsercizioModel;
+use yii\web\UploadedFile;
 use Yii;
 use yii\helpers\VarDumper;
 
@@ -15,34 +16,92 @@ class EsercizioController extends \yii\web\Controller
 
         $post = Yii::$app->request->post();
 
-        $iem = new ImmagineEsercizioModel();
+        Yii::error($post);
 
-        $iem->load($post);
+        if ($tipologiaEsercizio == 'let') {
 
-        //Yii::error($iem->attributes);
+            $model = new EsercizioModel();
 
-        if (isset($post['continue-button'])) {
+            if ($model->load($post)) {
 
-            // TODO salva le immagini
+                Yii::error($model->attributes);
+
+                $model->save();
+            }
+
+
+            return $this->render(
+                'creaesercizio',
+                [
+                    'tipologiaEsercizio' => $tipologiaEsercizio
+                ]
+            );
+        } else if ($tipologiaEsercizio == 'abb') {
+
+            $path = realpath("esercizi");
+
+            if ($path == false || !is_dir($path)) {
+                mkdir($directory = "esercizi", $recursive = true);
+            }
+
+
+            $iem = new ImmagineEsercizioModel();
+
+            $iem->load($post);
+
+            //Yii::error($iem->attributes);
+
+            if (isset($post['continue-button'])) {
+
+                $path = realpath("esercizi/$iem->nomeEsercizio");
+
+                if ($path == false || !is_dir($path)) {
+                    mkdir("esercizi/$iem->nomeEsercizio");
+                } else {
+                }
+
+                $iem->file = UploadedFile::getInstance($iem, 'file');
+                $iem->file->saveAs("esercizi/$iem->nomeEsercizio/$iem->nomeImmagine.jpg");
+
+                return $this->render(
+                    'creaesercizio',
+                    [
+                        'tipologiaEsercizio' => $tipologiaEsercizio,
+                        'nomeEsercizio' => $iem->nomeEsercizio,
+                        'model' => new ImmagineEsercizioModel(),
+                    ]
+                );
+            } else if (isset($post['end-button'])) {
+
+                $path = realpath("esercizi/$iem->nomeEsercizio");
+
+                if ($path == false || !is_dir($path)) {
+                    mkdir("esercizi/$iem->nomeEsercizio");
+                }
+
+                $iem->file = UploadedFile::getInstance($iem, 'file');
+                $iem->file->saveAs("esercizi/$iem->nomeEsercizio/$iem->nomeImmagine.jpg");
+
+                $model = new EsercizioModel();
+                $model->nome = $iem->nomeEsercizio;
+                $model->tipologia = $tipologiaEsercizio;
+                $model->path = "esercizi/$iem->nomeEsercizio";
+                $model->logopedista = Yii::$app->user->getId();
+
+                $model->save();
+
+
+                //return $this->redirect('logopedista/dashboardlogopedista?tipoAttore=log');
+            }
+
             return $this->render(
                 'creaesercizio',
                 [
                     'tipologiaEsercizio' => $tipologiaEsercizio,
-                    'nomeEsercizio' => $iem->nomeEsercizio,
+                    'nomeEsercizio' => null,
                     'model' => new ImmagineEsercizioModel(),
                 ]
             );
-        } else if(isset($post['end-button'])) {
-
         }
-
-        return $this->render(
-            'creaesercizio',
-            [
-                'tipologiaEsercizio' => $tipologiaEsercizio,
-                'nomeEsercizio' => null,
-                'model' => new ImmagineEsercizioModel(),
-            ]
-        );
     }
 }
--- basic/views/esercizio/creaesercizio.php
+++ basic/views/esercizio/creaesercizio.php
@@ -31,9 +31,9 @@ $this->params['breadcrumbs'][] = $this->title;
             echo $form->field($model, 'file')->fileInput();
 
             echo Html::submitButton('Conferma', ['class' => 'btn btn-primary mr-1',  'name' => 'continue-button']);
-        } else if($nomeEsercizio != null) {
+        } else if ($nomeEsercizio != null) {
 
-            echo $form->field($model, 'nomeEsercizio')->textInput()->label($nomeEsercizio);
+            echo $form->field($model, 'nomeEsercizio')->textInput(['readonly' => true, 'value' => $nomeEsercizio]);
             echo $form->field($model, 'nomeImmagine')->textInput()->label('Nome Immagine');
             echo $form->field($model, 'file')->fileInput();
 
@@ -41,6 +41,22 @@ $this->params['breadcrumbs'][] = $this->title;
             echo Html::submitButton('Termina inserimento', ['class' => 'btn btn-primary',  'name' => 'end-button']);
         }
     } else if ($tipologiaEsercizio == 'let') {
+
+        $esModel = new EsercizioModel();
+        echo $form->field($esModel, 'nome')->textInput()->label('Nome esercizio');
+        echo $form->field($esModel, 'testo')->textInput()->label('Testo');
+
+        $hiddenFieldLogopedista = $form->field($esModel, 'logopedista')
+            ->hiddenInput(['value' => Yii::$app->user->getId()])->label(false);
+
+        echo $hiddenFieldLogopedista;
+
+        $hiddenFieldTipologia = $form->field($esModel, 'tipologia')
+            ->hiddenInput(['value' => 'let'])->label(false);
+
+        echo $hiddenFieldTipologia;
+
+        echo Html::submitButton('Conferma', ['class' => 'btn btn-primary mr-1',  'name' => 'continue-button']);
     }
     ?>
 
